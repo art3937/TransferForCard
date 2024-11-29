a618dd32cd41aaeb9d4c690e21c0c186369cf66a
Artiom test <art3937@gmail.com>
2024-11-30 00:14:25 +0300

    for pull request

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,10 +1,18 @@
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Test
 
-class MainKtTest {
+import org.junit.Assert.*
+ class MainKtTest {
 
 @Test
- fun transferMoney() {
-  assertEquals(75,transferMoney("Мир",1000,10000))
- }
+fun toMastercard() {
+ assertEquals(26,toMastercard(75000,1000))
 }
+  @Test
+  fun toMastercardNotSale() {
+   assertEquals(0,toMastercard(1000,1000))
+  }
+     @Test
+     fun toMastercardMore() {
+         assertEquals(toMastercard(100,100000),20)
+     }
+ }
